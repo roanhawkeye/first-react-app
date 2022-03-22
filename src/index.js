5e8ae8bfdce57a53abf968542ed86c1a69700eec
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-22 14:54:10 -0600

    Add some sample html into jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 //Import the React and ReactDOM Libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 //Create a react component
-const App = function(){
-    return <div>Hi there!</div>
+const App = () => {
+  const buttonText = "Click me!";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
